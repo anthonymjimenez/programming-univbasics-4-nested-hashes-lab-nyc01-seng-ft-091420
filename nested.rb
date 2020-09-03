ae081d727b2a08b5119e1fe5e0d297a0648d38d2
Anthony Jimenez <anthonyjim96@gmail.com>
2020-09-03 01:24:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -141,7 +141,7 @@ def adding_to_dennis
       }
     }
 
-    programmer_hash[:dennis_ritchie].push("Assembly")
+    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 
     programmer_hash
 end
